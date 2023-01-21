10509e1d2829a5ecc3d1af0b3f632c618e3c7baf
anikinkirill <anikinkirill@yandex-team.ru>
2023-01-21 11:06:26 +0700

    init commit in AndroidStudioSetup folder

---
+++ AndroidStudioSetup/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Application"
+include(":androidApp")
+include(":shared")
