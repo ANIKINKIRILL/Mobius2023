735523f763a262322286c9588ec54913fb586cd5
anikinkirill <anikinkirill@yandex-team.ru>
2023-01-21 11:13:46 +0700

    init commit in IntellijIdeaSetup folder

---
+++ IntelliJIdeaSetup/ComposeMultiplatform/common/src/commonMain/kotlin/com/example/mobius/common/platform.kt
@@ -0,0 +1,3 @@
+package com.example.mobius.common
+
+expect fun getPlatformName(): String
---
+++ IntelliJIdeaSetup/ComposeMultiplatform/common/src/desktopMain/kotlin/com/example/mobius/common/platform.kt
@@ -0,0 +1,5 @@
+package com.example.mobius.common
+
+actual fun getPlatformName(): String {
+    return "Desktop"
+}
