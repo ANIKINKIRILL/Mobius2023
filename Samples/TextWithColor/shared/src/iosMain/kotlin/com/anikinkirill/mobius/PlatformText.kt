27c0d0258a0a82c9ee45f1467183f0d28c230f69
anikinkirill <anikinkirill@yandex-team.ru>
2023-01-24 19:14:02 +0700

    Use fontStyle, fontWeight, textDecoration in iOS PlatformText

--- Samples/TextWithColor/shared/src/iosMain/kotlin/com/anikinkirill/mobius/PlatformText.kt
+++ Samples/TextWithColor/shared/src/iosMain/kotlin/com/anikinkirill/mobius/PlatformText.kt
@@ -1,10 +1,19 @@
 package com.anikinkirill.mobius
 
-import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 
 @Composable
 internal actual fun PlatformText() {
-    Text(text = Greeting().greeting(), color = Color.Blue)
+    Text(
+        text = Greeting().greeting(),
+        color = Color.Blue,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.LineThrough,
+    )
 }
